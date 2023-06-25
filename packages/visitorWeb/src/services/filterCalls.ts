59914d4bbf6ae8076e190e6932991aa539b34a7d
wernerramon <ramon.werner@epitech.eu>
2023-06-25 15:39:00 +0200

    [BE] [MODIFY] fixed cors error

--- packages/visitorWeb/src/services/filterCalls.ts
+++ packages/visitorWeb/src/services/filterCalls.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const baseUrl = 'http://localhost:8081/api/filter/';
-const selectedURL = 'http://localhost:8081/api/filteredlist';
+const selectedURL = 'http://localhost:8081/api/filter/filteredlist';
 
 export const getFilteredRestos = async (body: any) => {
     try {
