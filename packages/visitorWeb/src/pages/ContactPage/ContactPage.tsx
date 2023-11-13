46789eec98e0962d373e82faec70c51bb6b8ac2b
Ramon Werner <ramon.werner@epitech.eu>
2023-11-13 13:01:34 +0000

    [FE] [ADD] add scaffolding for contact page

---
+++ packages/visitorWeb/src/pages/ContactPage/ContactPage.tsx
@@ -0,0 +1,13 @@
+import React, { useEffect } from "react";
+import styles from "./ContactPage.module.scss";
+import Header from "@src/components/Header/Header";
+
+const ContactPage = () => {
+    return (
+        <div>
+            <Header />
+        </div>
+    )
+};
+
+export default ContactPage;
