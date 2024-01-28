235b9f384efbcd82597067f29c49cd4e76e7b76b
RenanDbs <71699658+RenanDbs@users.noreply.github.com>
2024-01-28 02:58:28 +0100

    Eip 221 mobile menu page (#47)
    * Resto view + Add/delete/Edit pages
    
    * WIP : Menu pages for restoApp
    
    * [MOBILE] Fixed package.json
    
    * [MOBILE] [ADD] Menu page for RestoMobile
    
    * [MOBILE] [FIX] Fix the styles for menu pages so it's complient with the rest of the mobile architecture
    
    ---------
    
    Co-authored-by: Dubois Renan <duboisrenan@MacBook-Air-de-Duboi.local>

--- packages/restoMobile/Router.tsx
+++ packages/restoMobile/Router.tsx
@@ -50,7 +50,7 @@ const MyTabs = () => {
         })}
       >
         <Tab.Screen name="Scanning" component={MyQrStack} />
-        <Tab.Screen name="My Restaurants" component={MyRestaurantsScreen} />
+        <Tab.Screen name="My Restaurants" component={MyStack} />
         <Tab.Screen name="My Dishes" component={MyDishesScreen} />
         <Tab.Screen name="My Products" component={MyProductStack} />
         <Tab.Screen name="Login" component={LoginScreen} />
@@ -63,14 +63,14 @@ const MyQrStack = () => {
   return (
     <Stack.Navigator>
       <Stack.Screen
-        name="QRCodeEngin"
-        component={QRCodeEngin}
-        options={{headerShown: false}}
-      />
-      <Stack.Screen
         name="AddPage"
         component={AddPage}
         options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="QRCodeEngin"
+        component={QRCodeEngin}
+        options={{headerShown: false}}
       />
     </Stack.Navigator>
   )
